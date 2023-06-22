3f54f506c33592fbe7ebcd066c49e55400510789
Sameer Mirza <srmirza14@gmail.com>
2023-06-21 22:02:37 -0400

    created tables in schema

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- table creation
+
+-- department
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+-- role
+CREATE TABLE  role (
+    id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES  department(id)
+);
+
+-- employee
+CREATE TABLE employee (
+    id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name DECIMAL NOT NULL,
+    department_id INT,
+    manager_id INT,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
